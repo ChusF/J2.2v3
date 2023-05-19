a323012ee93f97d1cb3a423e950f6ebd4ee7fcf3
Nadezhda Krivorotova <chaus-fox@list.ru>
2023-05-19 11:51:47 +0600

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 class Main {
     public static void main(String[] args) {
 
-        int first = 2000; // сумма на счету
-        int summa = 1900; // сумма пополнения
+        int first = 100; // сумма на счету
+        int summa = 1000; // сумма пополнения
         int condition = 100;
 
-        if (summa >= 1000) {
+        if (summa > 1000) {
             int bonus = (summa / condition);
             System.out.println("Начисленно бонусов:");
-            System.out.println(summa / condition);
+            System.out.println(bonus);
             System.out.println("Сумма на счету:");
             System.out.println(first + bonus + summa);
+
         } else {
             System.out.println("Сумма на счету:");
             System.out.println(first + summa);
